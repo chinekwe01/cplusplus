2027a12432716f1ceaaa9a95eea2dbfd44a3f3ea
codewithchinekwe <codewithchinekwe@gmail.com>
2022-11-27 06:21:01 +0100

    added comment for each line of code

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //header file library
+using namespace std;    // use names for objects and variable from the standard library
 
-int main(){
-    cout << "Hello World" << "\n";
-    return 0;
+int main(){ //function, code inside { } would be executed
+    cout << "Hello World" << "\n";  //an object used to output/ print text, in this case "Hello World"
+    return 0; //Ends or terminates the main function
 }
 
 
