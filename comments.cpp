769ebfb9fa8ac82a2e2c222c4e63c3b5f72c7639
codewithchinekwe <codewithchinekwe@gmail.com>
2022-11-27 06:42:48 +0100

    inline and multiline comment in c++

---
+++ comments.cpp
@@ -0,0 +1,22 @@
+/*
+
+Comments can be used to explain C++ code, and to make it more readable.
+It can also be used to prevent execution when testing alternative code.
+Comments can be singled-lined or multi-lined.
+Single-line comments start with two forward slashes (//).
+Any text between // and the end of the line is ignored by the compiler (will not be executed).
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  // This is a comment
+  cout << "Hello World!" << "\n";
+
+
+  /* The code below will print the words Hello World!
+  to the screen, and it is amazing */
+  cout << "Hello World!" << "\n";
+  return 0;
+}
